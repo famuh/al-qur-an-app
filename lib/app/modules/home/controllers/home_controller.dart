245bfc64ecb8eb440b1548a4da0c932586b31fb1
famuh <famuh79@gmail.com>
2024-04-01 16:40:47 +0700

    menampilkan list surah di home

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,8 +1,25 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../data/models/surah.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  Future<List<Surah>> getAllSurah() async {
+    Uri url = Uri.parse('https://api.quran.gading.dev/surah');
+    var res = await http.get(url);
 
+    List data = (jsonDecode(res.body) as Map<String, dynamic>)["data"];
+
+     if (data.isEmpty) {
+       return [];
+     } else {
+      return data.map((e) => Surah.fromJson(e)).toList();
+     }
+  }
+
+// default
   final count = 0.obs;
   @override
   void onInit() {
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../data/models/surah.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -9,16 +10,35 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('HomeView'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: FutureBuilder<List<Surah>>(
+          future: controller.getAllSurah(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator(),);
+            }
+            if (!snapshot.hasData) {
+              return const Center(child: Text("Tidak ada data"),);
+            }
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index) {
+                Surah surah = snapshot.data![index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Text("${index + 1}"),
+                  ),
+                  title: Text(surah.name.transliteration.id),
+                  subtitle: Text("${surah.numberOfVerses} Ayat | ${surah.revelation.id.toString().split(".").last.capitalizeFirst}"),
+                  trailing: Text(surah.name.short),
+                  onTap: (){},
+                );
+              },
+            );
+          },
+        ));
   }
 }
