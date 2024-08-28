705275f58b37d8d3ffc02c3025774b203447647c
famuh <famuh79@gmail.com>
2024-08-28 20:51:19 +0700

    add avatar list border and tab at home_view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -4,6 +4,7 @@ import 'package:alquran_app/app/routes/app_pages.dart';
 import 'package:alquran_app/common/colors.dart';
 import 'package:alquran_app/common/space.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:get/get.dart';
 
@@ -27,123 +28,181 @@ class HomeView extends GetView<HomeController> {
                 icon: const Icon(Icons.search))
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-               Text(
-                "Assalamualaikum",
-                style:  TextStyle(
-                  fontSize: 16,
-                  color: appPurpleLight1
-                  // fontWeight: FontWeight
+        body: DefaultTabController(
+          length: 3,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Assalamualaikum",
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: appPurpleLight1,
+                      fontWeight: FontWeight.w500),
                 ),
-              ),
-               const Text(
-                "Fadhil Muhammad",
-                style:
-                     const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, ),
-              ),
-              spaceH10,
-              InkWell(
-                onTap: (){
-                  Get.defaultDialog(
-                    content: const Text("belum diimplementasikan")
-                  );
-                },
-                child: Container(
-                  height: 120,
-                  width: Get.width,
-                  padding: const EdgeInsets.only(left: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(20),
-                      gradient: LinearGradient(
-                          colors: [appPurpleLight1, appPurpleDark])),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                          bottom: -40,
-                          right: -10,
-                          child: SizedBox(
-                            width: 140,
-                            height: 140,
-                            child: Image.asset(
-                              'assets/images/alquran.png',
-                              fit: BoxFit.contain,
-                            ),
-                          )),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.menu_book_outlined,
-                                color: appWhite,
-                              ),
-                              spaceW10,
-                              Text(
-                                "Terakhir Dibaca",
-                                style: TextStyle(color: appWhite),
-                              )
-                            ],
-                          ),
-                          spaceH10,
-                          Text(
-                            "Al-Fatihah",
-                            style: TextStyle(color: appWhite, fontSize: 18, fontWeight: FontWeight.w500),
-                          ),
-                          spaceH10,
-                          Text(
-                            "Juz 1 | Ayat 5",
-                            style: TextStyle(color: appWhite),
-                          )
-                        ],
-                      ),
-                    ],
+                const Text(
+                  "Fadhil Muhammad",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
-              )
-            ],
+                spaceH10,
+                InkWell(
+                  onTap: () {
+                    Get.toNamed(Routes.LAST_READ);
+                  },
+                  child: Container(
+                    height: 120,
+                    width: Get.width,
+                    padding: const EdgeInsets.only(left: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(
+                            colors: [appPurpleLight1, appPurpleDark])),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                            bottom: -40,
+                            right: -10,
+                            child: SizedBox(
+                              width: 140,
+                              height: 140,
+                              child: Image.asset(
+                                'assets/images/alquran.png',
+                                fit: BoxFit.contain,
+                              ),
+                            )),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.menu_book_outlined,
+                                  color: appWhite,
+                                ),
+                                spaceW10,
+                                Text(
+                                  "Terakhir Dibaca",
+                                  style: TextStyle(color: appWhite),
+                                )
+                              ],
+                            ),
+                            spaceH10,
+                            Text(
+                              "Al-Fatihah",
+                              style: TextStyle(
+                                  color: appWhite,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            spaceH10,
+                            Text(
+                              "Juz 1 | Ayat 5",
+                              style: TextStyle(color: appWhite),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                spaceH10,
+                TabBar(
+                    dividerColor: Colors.transparent,
+                    unselectedLabelColor: appPurpleLight2,
+                    tabs: [
+                      const Tab(
+                        child: Text("Surah"),
+                      ),
+                      const Tab(
+                        child: Text("Juz"),
+                      ),
+                      const Tab(
+                        child: Text("Bookmark"),
+                      ),
+                    ]),
+                Expanded(
+                  child: TabBarView(children: [
+                    // list surah
+                    FutureBuilder<List<Surah>>(
+                      future: controller.getAllSurah(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        if (!snapshot.hasData) {
+                          return const Center(
+                            child: Text("Tidak ada data"),
+                          );
+                        }
+                        return ListView.builder(
+                          itemCount: snapshot.data?.length,
+                          itemBuilder: (context, index) {
+                            Surah surah = snapshot.data![index];
+                            return ListTile(
+                              leading: Container(
+                                width: 35,
+                                height: 35,
+                                decoration: const BoxDecoration(
+                                  image:  DecorationImage(image: AssetImage('assets/images/avatar-border-list.png'),)
+                                ),
+                                child: Center(child: Text(surah.number.toString(), style: const TextStyle(
+                                  fontWeight: FontWeight.bold
+                                ),))
+                              ),
+                              title: Text(surah.name!.transliteration!.id),
+                              subtitle: Text(
+                                  "${surah.numberOfVerses} Ayat | ${surah.revelation?.id.toString().split(".").last.capitalizeFirst}"),
+                              trailing: Text(surah.name!.short!),
+                              onTap: () {
+                                Get.toNamed(Routes.DETAIL_SURAH,
+                                    arguments: surah);
+                              },
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  
+                  
+                  ListView.builder(
+                    itemCount: 30,
+                    itemBuilder: (context, index) {
+                    return ListTile(
+                              leading: Container(
+                                width: 35,
+                                height: 35,
+                                decoration: const BoxDecoration(
+                                  image:  DecorationImage(image: AssetImage('assets/images/avatar-border-list.png'),)
+                                ),
+                                child: Center(child: Text("${index+1}", style: const TextStyle(
+                                  fontWeight: FontWeight.bold
+                                ),))
+                              ),
+                              title: const Text("abc"),
+                              subtitle: const Text(
+                                  "abc Ayat | surah.revelation?.id.toString().split(.).last.capitalizeFirst"),
+                              trailing: const Text("abc"),
+                              onTap: () {
+                                // belum diimplementasikan
+                              },
+                            );
+                  },),
+                    const Center(child: const Text("3")),
+                  ]),
+                )
+              ],
+            ),
           ),
-        )
-        // FutureBuilder<List<Surah>>(
-        //   future: controller.getAllSurah(),
-        //   builder: (context, snapshot) {
-        //     if (snapshot.connectionState == ConnectionState.waiting) {
-        //       return const Center(
-        //         child: CircularProgressIndicator(),
-        //       );
-        //     }
-        //     if (!snapshot.hasData) {
-        //       return const Center(
-        //         child: Text("Tidak ada data"),
-        //       );
-        //     }
-        //     return ListView.builder(
-        //       itemCount: snapshot.data?.length,
-        //       itemBuilder: (context, index) {
-        //         Surah surah = snapshot.data![index];
-        //         return ListTile(
-        //           leading: CircleAvatar(
-        //             child: Text("${index + 1}"),
-        //           ),
-        //           title: Text(surah.name!.transliteration!.id),
-        //           subtitle: Text(
-        //               "${surah.numberOfVerses} Ayat | ${surah.revelation?.id.toString().split(".").last.capitalizeFirst}"),
-        //           trailing: Text(surah.name!.short!),
-        //           onTap: () {
-        //             Get.toNamed(Routes.DETAIL_SURAH, arguments: surah);
-        //           },
-        //         );
-        //       },
-        //     );
-        //   },
-        // )
-
-        );
+        ));
   }
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -7,6 +7,7 @@ abstract class Routes {
   static const HOME = _Paths.HOME;
   static const DETAIL_SURAH = _Paths.DETAIL_SURAH;
   static const SEARCH = _Paths.SEARCH;
+  static const LAST_READ = _Paths.LAST_READ;
 }
 
 abstract class _Paths {
@@ -15,4 +16,5 @@ abstract class _Paths {
   static const HOME = '/home';
   static const DETAIL_SURAH = '/detail-surah';
   static const SEARCH = '/search';
+  static const LAST_READ = '/last-read';
 }
