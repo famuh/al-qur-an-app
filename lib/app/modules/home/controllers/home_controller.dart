f8e4eddef1be707c9e3a6ded3c5f3d9b640b410a
famuh <famuh79@gmail.com>
2024-08-31 17:18:39 +0700

    Membuat model dan fungsi mendapatkan spesifik juz

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
 
+import 'package:alquran_app/app/data/models/juz.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 import '../../../data/models/surah.dart';
 
 class HomeController extends GetxController {
-RxBool isDark = false.obs;
-
+  RxBool isDark = false.obs;
 
   Future<List<Surah>> getAllSurah() async {
     Uri url = Uri.parse('https://api.quran.gading.dev/surah');
@@ -15,11 +15,27 @@ RxBool isDark = false.obs;
 
     List data = (jsonDecode(res.body) as Map<String, dynamic>)["data"];
 
-     if (data.isEmpty) {
-       return [];
-     } else {
+    if (data.isEmpty) {
+      return [];
+    } else {
       return data.map((e) => Surah.fromJson(e)).toList();
-     }
+    }
+  }
+
+ Future<Juz> getJuz(int juz) async {
+    Uri url = Uri.parse('https://api.quran.gading.dev/juz/$juz');
+    var res = await http.get(url);
+
+    if (res.statusCode == 200) { // Pastikan respons berhasil
+      Map<String, dynamic> data =
+          (jsonDecode(res.body) as Map<String, dynamic>)["data"];
+      Juz juzData = Juz.fromJson(data);
+
+      return juzData;
+    } else {
+      // Bisa lempar exception atau mengembalikan data default
+      throw Exception('Failed to load Juz');
+    }
   }
 
 // default
